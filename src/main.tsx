c6f16ee67eb516b1a1dcb48ab1318099fdbbc55c
MujahedSaleem <mujahed19971@gmail.com>
2025-07-19 00:09:17 +0300

    fix loader

--- src/main.tsx
+++ src/main.tsx
@@ -1,50 +1,37 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { ThemeProvider } from '@material-tailwind/react';
-import { AuthProvider } from './context/AuthContext.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { getSavedRoute } from './utils/routeStateManager';
+export function register(config?: any) {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Provider store={store}>
-      <BrowserRouter> {/* Only one Router here */}
-        <AuthProvider>
-          <ThemeProvider>
-            <App /> {/* App is wrapped by BrowserRouter here */}
-          </ThemeProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </Provider>
-  </StrictMode>
-);
+      navigator.serviceWorker.register(swUrl).then(registration => {
+        if (registration.waiting) {
+          // Immediately update the page
+          config?.onUpdate?.(registration);
+        }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onSuccess: (registration) => {
-    console.log('Service worker registration successful', registration);
-  },
-  onUpdate: (registration) => {
-    console.log('New content is available, please refresh', registration);
-    // You could show a notification here that a new version is available
-  },
-  onOffline: () => {
-    console.log('App is now offline');
-    // The app is offline, we could show a notification here
-  },
-  onOnline: () => {
-    console.log('App is back online');
-    // The app is back online, we could restore state here
-    const savedRoute = getSavedRoute();
-    if (savedRoute && window.location.pathname !== savedRoute) {
-      window.location.href = savedRoute;
-    }
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if (installingWorker) {
+            installingWorker.onstatechange = () => {
+              if (
+                installingWorker.state === 'installed' &&
+                navigator.serviceWorker.controller
+              ) {
+                // New update available
+                config?.onUpdate?.(registration);
+              }
+            };
+          }
+        };
+
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+          window.location.reload();
+        });
+
+        config?.onSuccess?.(registration);
+      }).catch(error => {
+        console.error('Error during service worker registration:', error);
+      });
+    });
   }
-});
+}
