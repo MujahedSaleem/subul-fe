dd4882a500adb572aa0db31855d24fb3eae2f27f
MujahedSaleem <mujahed19971@gmail.com>
2025-08-03 21:38:52 +0300

    open in dricing mode

--- src/utils/geo_utils.ts
+++ src/utils/geo_utils.ts
@@ -2,7 +2,7 @@ export function openGoogleMapsApp(lat: number, lng: number) {
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 	const appUrl = `comgooglemaps://?q=${lat},${lng}`;
-	const webUrl = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`;
+	const webUrl = `https://www.google.com/maps/search/?api=1&query=${lat},${lng}&directionsmode=driving`;
 
     if (isMobileDevice) {
         window.location.href = appUrl;
